8a9aa314401ad25cdaf00bbaf89dc4f77ffd0cdb
ismael1987 <ismael1987@example.com>
2018-03-28 23:28:05 +0200

    morse coder

---
+++ morse/src/main/java/com/Mp8TheMorseEncoder/morse/MorseApplication.java
@@ -0,0 +1,12 @@
+package com.Mp8TheMorseEncoder.morse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorseApplication.class, args);
+	}
+}
